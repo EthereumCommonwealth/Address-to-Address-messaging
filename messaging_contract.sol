7ac35bdcc1a35ede18a4f483421f7205b67dc745
Dexaran <dexaran820@gmail.com>
2017-12-19 16:38:36 +0000

    Update and rename CEW_msg_contract.sol to messaging_contract.sol

--- messaging_contract.sol
+++ messaging_contract.sol
@@ -7,8 +7,16 @@ contract ClassicEtherWallet_Messages {
         address from;
         string  text;
     }
+    
+    struct public_key_struct
+    {
+        string key;
+        string key_type;
+    }
+    
     mapping (address => uint256) public last_msg_index;
     mapping (address => mapping (uint256 => message)) public messages;
+    mapping (address => public_key_struct) public keys;
     
     function sendMessage(address _to, string _text)
     {
@@ -22,13 +30,24 @@ contract ClassicEtherWallet_Messages {
         return last_msg_index[_owner];
     }
     
-    function showLastMessage(address _who) constant returns (address, string)
+    function getLastMessage(address _who) constant returns (address, string)
     {
         return (messages[_who][last_msg_index[_who]].from, messages[_who][last_msg_index[_who]].text);
     }
     
-    function showMessageByIndex(address _who, uint256 _index) constant returns (address, string)
+    function getMessageByIndex(address _who, uint256 _index) constant returns (address, string)
     {
         return (messages[_who][_index].from, messages[_who][_index].text);
+    }
+    
+    function getPublicKey(address _who) constant returns (string _key, string _key_type)
+    {
+        return (keys[_who].key, keys[_who].key_type);
+    }
+    
+    function setPublicKey(string _key, string _type)
+    {
+        keys[msg.sender].key = _key;
+        keys[msg.sender].key_type = _type;
     }
 }
